4e90b602bd75ae4cc234c58d304dabeacd344c3d
StrikerRUS <nekit94-12@hotmail.com>
2018-06-06 05:19:23 +0300

    test

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -19,6 +19,7 @@ else
         sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test;
         sudo apt-get update;
         sudo apt-get install g++-5;
+        sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
         export CXX=g++-5 && export CC=gcc-5;
     fi
     wget -O conda.sh https://repo.continuum.io/miniconda/Miniconda${PYTHON_VERSION:0:1}-latest-Linux-x86_64.sh
